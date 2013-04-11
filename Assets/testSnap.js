ca1fffd34a8431a2b17cbbdeab2f398ecd77e52b
David Griffen - Kralkatorrik <dg4christ08@gmail.com>
2013-04-10 21:17:54 -0400

    Small commit, messing with testSnap - DG

--- Assets/testSnap.js
+++ Assets/testSnap.js
@@ -6,8 +6,12 @@ var cubeB : GameObject;
 var snapA : Transform;
 var snapB : Transform;
 
+var dist : float;
+var newPos : Vector3;
+
 function Start () {
-	
+	cubeA = GameObject.Find("Cube1");
+	cubeB = GameObject.Find("Cube2");
 }
 
 function Update () {
@@ -19,8 +23,27 @@ function Update () {
 function onMouseDown(){
 	var hit : RaycastHit;
 	var dir : Vector3;
+	var ray : Ray;
+	var cubeAOrig : Vector3;
 	
-	if (Physics.Raycast (camera.main.ScreenPointToRay(Input.mousePosition), hit)){
-		Debug.Log("Got something");
+	ray = camera.main.ScreenPointToRay(Input.mousePosition);
+	while(Input.GetButton("Fire1") && Physics.Raycast(ray, hit)){
+		if (hit.collider.gameObject == cubeA){
+			Debug.Log("Got cubeA");
+			cubeAOrig = cubeA.transform.position;
+			
+			
+			
+			//dist = Vector3.Distance(hit.transform.position, Camera.main.transform.position);
+			//newPos = ray.GetPoint(dist);
+			//hit.transform.position = newPos;
+			//hit.transform.position.z = cubeAOrig.z;
+			
+			
+			}
+		if (hit.collider.gameObject == cubeB)
+			Debug.Log("Got cubeB");
+			
+		yield;
 	}
 }
